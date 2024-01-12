11c8bf44e76e959e3115f4ec495b050c4324ff37
LancerAC <ilya.pashchenko.04@inbox.ru>
2024-01-12 20:42:32 +0300

    Entity - replace constructors

--- src/main/java/com/example/productcontroller/Entity/Product.java
+++ src/main/java/com/example/productcontroller/Entity/Product.java
@@ -4,14 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 @Entity
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,15 +19,10 @@ public class Product {
     private String name;
     private Short amount; //кол-во
 
-    public Product() {
-
-    }
 
     public void setId(Long id) {
         this.id = id;
     }
 
-    public Long getId() {
-        return id;
-    }
+
 }
