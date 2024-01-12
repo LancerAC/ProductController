87bad15100cf2c49d21fc1742cf4d5c3155127bc
LancerAC <ilya.pashchenko.04@inbox.ru>
2024-01-12 20:29:35 +0300

    Controller - create 'ProductController'

---
+++ src/main/java/com/example/productcontroller/Controller/ProductController.java
@@ -0,0 +1,38 @@
+package com.example.productcontroller.Controller;
+
+import com.example.productcontroller.DTO.ProductDTO;
+import com.example.productcontroller.Entity.Product;
+import com.example.productcontroller.Service.ProductService;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class ProductController {
+    //Controller классы:
+    //Обрабатывают HTTP запросы и управляют
+
+    private final ProductService productService;
+    @PostMapping
+    public ResponseEntity<Product> create(@RequestBody ProductDTO productDTO){
+        return new ResponseEntity<>(productService.create(productDTO), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> readAll(){
+        return new ResponseEntity<>(productService.readAll(), HttpStatus.OK);
+    }
+    @PutMapping
+    public ResponseEntity<Product> update(@RequestBody Product product){
+        return new ResponseEntity<>(productService.update(product), HttpStatus.OK);
+    }
+    @DeleteMapping("/{id}")
+    public HttpStatus deleteById(@PathVariable Long id){
+        productService.deleteById(id);
+        return HttpStatus.OK;
+    }
+}
