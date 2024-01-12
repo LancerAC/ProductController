de8c60dbb88792462efb3a2d7263f9abcc6ceb05
LancerAC <ilya.pashchenko.04@inbox.ru>
2024-01-12 18:56:40 +0300

    Initial commit

---
+++ src/test/java/com/example/productcontroller/ProductControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
